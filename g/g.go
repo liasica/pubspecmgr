118206283b424806825f8ae0302c309b006bfaf0
liasica <magicrolan@qq.com>
2025-09-01 13:44:59 +0800

    chore: 添加GitHub action编译脚本

---
+++ g/g.go
@@ -0,0 +1,12 @@
+// Copyright (C) pubspecmgr. 2025-present.
+//
+// Created at 2025-08-31, by liasica
+
+package g
+
+var (
+	Version    = "1.0.0"
+	Commit     = "none"
+	CommitDate = "unknown"
+	TreeState  = "unknown"
+)
--- logger/logger.go
+++
@@ -1,5 +0,0 @@
-// Copyright (C) pubspecmgr. 2025-present.
-//
-// Created at 2025-08-31, by liasica
-
-package logger
